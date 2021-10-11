50bf4b179ef94a5b0e57356bc596caaf55dd8dfd
fariha <cse_2012020053@lus.ac.bd>
2021-10-11 23:12:28 +0600

    EmployeeInformation class added

---
+++ src/Employee/EmployeeInformation.java
@@ -0,0 +1,71 @@
+package Employee;
+
+public class EmployeeInformation {
+    int employee_id;
+    int salary;
+    int age;
+    int phone_number;
+    String name;
+    String address;
+    String email_id;
+
+
+    public int getEmployee_id() {
+        return employee_id;
+    }
+
+    public void setEmployee_id(int employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+}
+
+
