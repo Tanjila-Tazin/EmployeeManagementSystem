84fa810fbceb721c91a3da765f8ebcd41fb5416a
Summiya12 <cse_2012020077@lus.ac.bd>
2021-10-12 14:06:30 +0600

    Health class added.

---
+++ src/Employee/Health.java
@@ -0,0 +1,39 @@
+Package EmployeeManagementSystem;
+
+public class Health {
+    package Employee;
+
+    public class Health {
+        String bloodgroup;
+        String vaccinationStatus;
+        String disease;
+
+        public Health() {
+        }
+
+        public String getBloodgroup() {
+            return this.bloodgroup;
+        }
+
+        public void setBloodgroup(String bloodgroup) {
+            this.bloodgroup = bloodgroup;
+        }
+
+        public String getVaccinationStatus() {
+            return this.vaccinationStatus;
+        }
+
+        public void setVaccinationStatus(String vaccinationStatus) {
+            this.vaccinationStatus = vaccinationStatus;
+        }
+
+        public String getDisease() {
+            return this.disease;
+        }
+
+        public void setDisease(String disease) {
+            this.disease = disease;
+        }
+    }
+
+}
