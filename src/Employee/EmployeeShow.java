bc20ebf9efc22e6c1ea2d20cf7638882497f207f
Summiya12 <cse_2012020077@lus.ac.bd>
2021-10-14 22:32:26 +0600

    Main class addes

---
+++ src/Employee/EmployeeShow.java
@@ -0,0 +1,22 @@
+package EmployeeManagementSystem;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+public class Employee_Show {
+
+        public void viewFile(String s) throws Exception
+        {
+
+            File file = new File("file"+s+".txt");
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine())
+            {
+                System.out.println(sc.nextLine());
+            }
+        }
+
+
+}
---
+++ src/Employee/Main.java
@@ -0,0 +1,110 @@
+package EmployeeManagementSystem;
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Employee_Show epv =new Employee_Show();
+
+        int i=0;
+
+        while(i<6)
+        {
+            MainMenu mm = new MainMenu();
+            mm.menu();
+
+            System.out.print("\nPlease Enter choice :");
+            i=Integer.parseInt(sc.nextLine());
+
+            switch(i)
+            {
+                case 1:
+                {
+                    /** Creating class's object and calling Function using that object **/
+                    Employee_Add ep =new Employee_Add();
+                    ep.createFile();
+                    break;
+                }
+                case 2:
+                {
+                    System.out.print("\nPlease Enter Employee's ID :");
+                    String s=sc.nextLine();
+                    try
+                    {
+                        epv.viewFile(s);}
+                    catch(Exception e){System.out.println(e);}
+
+
+                    System.out.print("\nPress Enter to Continue...");
+                    sc.nextLine();
+
+                    break;
+                }
+
+                case 3:
+                {
+                    System.out.print("\nPlease Enter Employee's ID :");
+                    String s=sc.nextLine();
+                    Employee_Remove epr =new Employee_Remove();
+                    epr.removeFile(s);
+
+                    System.out.print("\nPress Enter to Continue...");
+                    sc.nextLine();
+
+                    break;
+                }
+                case 4:
+                {
+                    System.out.print("\nPlease Enter Employee's ID :");
+                    String I=sc.nextLine();
+                    try
+                    {
+                        epv.viewFile(I);
+                    }
+                    catch(Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                    Employee_Update epu = new Employee_Update();
+                    System.out.print("Please Enter the detail you want to Update :");
+                    System.out.print("\nFor Example :\n");
+                    System.out.println("If you want to Change the Name, then Enter Current Name and Press Enter. Then write the new Name then Press Enter. It will Update the Name.\n");
+                    String s=sc.nextLine();
+                    System.out.print("Please Enter the Updated Info :");
+                    String n=sc.nextLine();
+                    try
+                    {
+                        epu.updateFile(I,s,n);
+
+                        System.out.print("\nPress Enter to Continue...");
+                        sc.nextLine();
+
+                        break;
+                    }
+                    catch(IOException e)
+                    {
+                        System.out.println(e);
+                    }
+                }
+                case 5:
+                {
+                    CodeExit obj = new CodeExit();
+                    obj.out();
+                }
+                break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + i);
+            }
+        }
+    }
+
+
+    }
+
+
